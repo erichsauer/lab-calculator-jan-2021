bf8628a3580ef531747cef6ef5d1234d18821c2e
ERICH SAUER <erich.sauer@gmail.com>
2021-01-07 15:24:50 -0800

    adds more functionality and affirmation statement

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # lab-calculator-jan-2021
+
+<!-- 1. grab dom elements
+    - two inputs
+    - operator buttons
+    - result -->
+
+<!-- 2. print inputs to console -->
+<!-- - print values to console -->
+<!-- 3. add button functionality -->
+4. add button cool zone 
+    - log inputs
+    -  log results
+5. display result on user interface
--- app.js
+++ app.js
@@ -1,53 +1,53 @@
 // import functions and grab DOM elements
-const input1 = document.getElementById('input-1')
-const input2 = document.getElementById('input-2')
-const addButton = document.getElementById('add-button')
-const subButton = document.getElementById('sub-button')
-const multButton = document.getElementById('mult-button')
-const divButton = document.getElementById('div-button')
-const result = document.getElementById('result-div')
-const affirm = document.getElementById('affirmation')
-
+const input1 = document.getElementById('input-1');
+const input2 = document.getElementById('input-2');
+const addButton = document.getElementById('add-button');
+const subButton = document.getElementById('sub-button');
+const multButton = document.getElementById('mult-button');
+const divButton = document.getElementById('div-button');
+const result = document.getElementById('result-div');
+const affirm = document.getElementById('affirmation');
 
 // initialize state
 
 // set event listeners to update state and DOM
 addButton.addEventListener('click', () => {
-    // cool zone
-    const inputNumber = Number(input1.value)
-    const inputNumber2 = Number(input2.value)
-    const addResult = inputNumber + inputNumber2
+  // cool zone
+    const inputNumber = Number(input1.value);
+    const inputNumber2 = Number(input2.value);
+    const addResult = inputNumber + inputNumber2;
 
-    affirm.textContent = "🧠 Mathematical!! 💪🏼"
-    result.textContent = addResult
+    affirm.textContent = '🧠 Mathematical!! 💪🏼';
+    result.textContent = addResult;
 });
 
-addButton.addEventListener('click', () => {
-    // cool zone
-    const inputNumber = Number(input1.value)
-    const inputNumber2 = Number(input2.value)
-    const addResult = inputNumber + inputNumber2
+subButton.addEventListener('click', () => {
+  // cool zone
+    const inputNumber = Number(input1.value);
+    const inputNumber2 = Number(input2.value);
+    const subResult = inputNumber - inputNumber2;
 
-    affirm.textContent = "🧠 Mathematical!! 💪🏼"
-    result.textContent = addResult
+    affirm.textContent = '🧠 Mathematical!! 💪🏼';
+    result.textContent = subResult;
 });
 
-addButton.addEventListener('click', () => {
-    // cool zone
-    const inputNumber = Number(input1.value)
-    const inputNumber2 = Number(input2.value)
-    const addResult = inputNumber + inputNumber2
+multButton.addEventListener('click', () => {
+  // cool zone
+    const inputNumber = Number(input1.value);
+    const inputNumber2 = Number(input2.value);
+    const multResult = inputNumber * inputNumber2;
 
-    affirm.textContent = "🧠 Mathematical!! 💪🏼"
-    result.textContent = addResult
+    affirm.textContent = '🧠 Mathematical!! 💪🏼';
+    result.textContent = multResult;
 });
 
-addButton.addEventListener('click', () => {
-    // cool zone
-    const inputNumber = Number(input1.value)
-    const inputNumber2 = Number(input2.value)
-    const addResult = inputNumber + inputNumber2
+divButton.addEventListener('click', () => {
+  // cool zone
+    const inputNumber = Number(input1.value);
+    const inputNumber2 = Number(input2.value);
+    const divResult = inputNumber / inputNumber2;
+    const divResultRounded = Math.round(divResult * 100.0) / 100.0;
 
-    affirm.textContent = "🧠 Mathematical!! 💪🏼"
-    result.textContent = addResult
+    affirm.textContent = '🧠 Mathematical!! 💪🏼';
+    result.textContent = divResultRounded;
 });
